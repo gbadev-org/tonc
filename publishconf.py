0a33292d718ffe62f87728bc0a46d26b3d642ca7
Jeremy Clarke <gecko@exelo.tl>
2023-08-13 15:41:03 +0100

    Add deployment workflow

--- publishconf.py
+++ publishconf.py
@@ -7,15 +7,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'https://tonc.gbadev.net'
+SITEURL = 'https://gbadev.net/tonc'
 RELATIVE_URLS = False
 
-FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
-
 DELETE_OUTPUT_DIRECTORY = True
-
-# Following items are often useful when publishing
-
-#DISQUS_SITENAME = ""
-#GOOGLE_ANALYTICS = ""
